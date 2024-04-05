7eeb712224a0b917e75e5a577ecc8aaea79842b9
joancuni <joancuni2@gmail.com>
2024-04-05 12:48:11 +0200

    wip 'expansor'

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -10,16 +10,39 @@ void	ft_get_paths(char **envp, t_env *data)
 		i++;
 	data->paths = ft_split(&envp[i][5], ':');
 }
+/*-Pre-expansor: verifica si hay algun '$' en 'str' y devuelve TRUE or FALSE*/
+//Si la WORD contiene, en alguna posicion '$' llamaremos a la funcion 'expander'.
+int	check_for_dollar(nodo o char *str)
+{
+	size_t	i;
 
-Recibira como args (t_cmd **cmd, la copia del env, exit_status???)
-Si la WORD contiene, en alguna posicion '$' llamaremos a la funcion 'expander'.
+	i = 0;
+	while (str[i])
+	{
+
+
+
+	}
+
+
+
+}
+/*--Funcion que hace el expander--*/
+//Recibira como args (t_cmd **cmd, la copia del env, exit_status???)
+
+	-verifica si despues de '$' hay un '?'
 
 
 Despues del '$' para que sea una variable valida, 
 solo puede empezar por caracter alphabetico o '_', 
 solo puede contener caracteres alfabeticos (mayuscula o minuscula) o numericos o '_',
-y debe coincidir exactamente hasta el '=' con una de las variables del 'env'. 
+y debe coincidir exactamente hasta el '=' con una de las variables del 'env'.
+y ademas despues del nombre de la variable ha de haber un espacio (si no lo hay, 
+se ignora la variable y solo hace un salto de linea) 
 Si coincide y ademas tiene algun otro caracter alphabetico o numerico ya no seria valida.
+
+Si despues del '$' hay un '?' expande el exit_status del ultimo comando y si hay algun
+ caracter concatenado al '?' tambien lo muestra concatenado al exit_status
 
 '$' = bash: $: command not found
 
