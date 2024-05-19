a911b65c95156d173fe877a961c10add4371cbf6
joancuni <joancuni2@gmail.com>
2024-05-19 06:38:16 +0200

    fixed expander_and_quote_remove

--- src/expander/get_env_key.c
+++ src/expander/get_env_key.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-/*Retorna un nombre de variable mallocado o NULL si empieza por numero o caracter especial
+/*Retorna un posible nombre de variable mallocado
 No tiene en cuenta si existe, ni si su sintaxis es valida.*/
 char *get_env_key(char *str)
 {
@@ -9,14 +9,22 @@ char *get_env_key(char *str)
 
 	i = 0;
 	var_name = NULL;
-/*SI NO SON CARACTERES VALIDOS PARA UNA VARIABLE DE ENTORNO*/
+/*	
+//SI NO SON CARACTERES VALIDOS PARA UNA VARIABLE DE ENTORNO
 	if (!ft_isalpha(str[0]) && str[0] != '_')
 		return (NULL);
-/*SI SON CARACTERES VALIDOS, CUENTALOS, RESERVA MEMORIA Y RELLENALA CON SU VALOR*/
+	
+//SI SON CARACTERES VALIDOS, CUENTALOS, RESERVA MEMORIA Y RELLENALA CON SU VALOR
 	while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
 		i++;
 	var_name = (char *)p_malloc(sizeof(char) * (i + 1));
 	ft_strlcpy(var_name, str, i + 1);
 	return (var_name);
+*/
+//	while (str[i])
+	while (str[i] && str[i] != '\'' && str[i] != '"')
+		i++;
+	var_name = (char *)p_malloc(sizeof(char) * (i + 1));
+	ft_strlcpy(var_name, str, i + 1);
+	return (var_name);
 }
-/*OJO: aqui deberia devolver el key, sea valido o no, para poder contarlos y saltarlos*/
