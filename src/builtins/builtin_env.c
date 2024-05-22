1da7d4c9afd395d9482d71925bee88feb06f8fa1
Carlos Astorga <castorga@cbr1s6.42barcelona.com>
2024-05-22 15:34:23 +0200

    up builtin_env.c

--- src/builtins/builtin_env.c
+++ src/builtins/builtin_env.c
@@ -22,12 +22,15 @@ int	builtin_env(t_cmd *cmd, t_env *env)
 		if ((ft_strcmp(cmd->commands[1], "test") == 0) || \
 		(ft_strcmp(cmd->commands[1], "true") == 0) || \
 		(ft_strcmp(cmd->commands[1], "false") == 0))
-			return (0);
+			return (1);
 		else if (ft_strcmp(cmd->commands[1], "echo") == 0)
+		{
 			printf("\n");
+			return (0);
+		}
 		else
 			ft_msgs(2, cmd);
-		return (0);
+		return (1);
 	}
 	else
 		print_env(env);
