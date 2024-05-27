60b58d2c321b01cc5b179eb1c6fb2fbba1799265
joancuni <joancuni2@gmail.com>
2024-05-27 18:29:03 +0200

    'tokenizer' functions refactored to keep them bellow 25 lines

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -1,8 +1,10 @@
 #include "minishell.h"
 
-//Tokeniza la 'line' sea cual sea, sin verificar errores sintacticos (separa por ' ', \t, <, >, >>, <<, |)
-//'str' siempre sera NULL, excepto si es una WORD que contendra su string
-//las comillas se tratan como WORD y si no estan cerradas se interpreta que llegan hasta fin de linea
+/*Tokens the content of 'line' using delimiters (' ', \t, <, >, >>, <<, |) \
+and put them in a 'tok' list. Each node contains tok->type and tok->str if it\
+ is not an operator.
+Function does not check for syntax errors. If there are quotes and they are \
+not closed, they close at the end of the line.*/
 void	tokenizer(t_tok **tok, char *line)
 {
 	t_tok	*new_tok;
@@ -21,12 +23,6 @@ void	tokenizer(t_tok **tok, char *line)
 			new_tok = tok_new_node(NULL, NULL_TYPE);//malloco node inicialitzat a NULL
 			len = tok_len(line + i, &new_tok);//inicializa 'type' si es operador y retorna un 'len' > 0 si es una WORD
 			init_word_str(len, new_tok, line, i);//si len > ), inicializo tok->str con la palabra mallocada y tok->type con el enum 'WORD' 
-//			if (len > 0)//si len es > que 0, significa que hay una WORD. Reservo memoria y la meteo en tok->str. Meto 'WORD' en tok->type 
-//			{
-//				new_tok->str = (char *)p_malloc(sizeof(char) * (len + 1));//malloco *str dentro del nodo
-//				ft_strlcpy(new_tok->str, line + i, len + 1);
-//				new_tok->type = WORD;
-//			}
 			if (new_tok->type == DOUBLE_GREATER || new_tok->type == DOUBLE_SMALLER)
 				len += 2;//por el doble operador
 			if (new_tok->type == GREATER || new_tok->type == SMALLER || new_tok->type == PIPE)
