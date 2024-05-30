a0c44ce06520f2573270df84235e781d1a65995c
Carlos Astorga <castorga@cbr1s6.42barcelona.com>
2024-05-30 13:11:25 +0200

    should_expand function divided

--- src/expander/should_expand.c
+++ src/expander/should_expand.c
@@ -1,38 +1,40 @@
 #include "minishell.h"
 
+static void	should_expand_core(t_cmd *cmd_aux, t_env *envlist)
+{
+	size_t	i;
+	t_redir	*redir_aux;
+
+	redir_aux = cmd_aux->redir;
+	i = 0;
+	while (cmd_aux->commands && cmd_aux->commands[i])//busca $, ', " en los comandos
+	{
+		if (ft_strchr(cmd_aux->commands[i], '$') != NULL ||
+			ft_strchr(cmd_aux->commands[i], '\'') != NULL ||
+			ft_strchr(cmd_aux->commands[i], '"') != NULL)
+			cmd_aux->commands[i] = expand_quote_rm(cmd_aux->commands[i], \
+			envlist);
+		i++;
+	}
+	while (redir_aux)//busca ', ", $ en las redirecciones
+	{
+		if (ft_strchr(redir_aux->fname, '$') != NULL ||
+			ft_strchr(redir_aux->fname, '\'') != NULL ||
+			ft_strchr(redir_aux->fname, '"') != NULL)
+			redir_aux->fname = expand_quote_rm(redir_aux->fname, envlist);
+		redir_aux = redir_aux->next;
+	}
+}
+
 /* Look for any expandable or quote removable token into 'cmd' list */
 void	should_expand(t_cmd *cmd, t_env *envlist)
 {
-	size_t	i;
 	t_cmd	*cmd_aux;
-	t_redir	*redir_aux;
-//	char	*redir_tmp;
 
 	cmd_aux = cmd;
-	redir_aux = cmd->redir;
 	while (cmd_aux)
 	{
-		i = 0;
-		while (cmd_aux->commands && cmd_aux->commands[i])//busca $, ', " en los comandos
-		{
-			if (ft_strchr(cmd_aux->commands[i], '$') != NULL ||
-				ft_strchr(cmd_aux->commands[i], '\'') != NULL ||
-				ft_strchr(cmd_aux->commands[i], '"') != NULL)
-				cmd_aux->commands[i] = expand_quote_rm(cmd_aux->commands[i], envlist);
-			i++;
-		}
-		while (redir_aux)//busca ', ", $ en las redirecciones
-		{
-		 	if (ft_strchr(redir_aux->fname, '$') != NULL ||
-				ft_strchr(redir_aux->fname, '\'') != NULL ||
-				ft_strchr(redir_aux->fname, '"') != NULL)
-//		 	{
-//	 			redir_tmp = expand_quote_rm(redir_aux->filename, envlist);
-//		 		redir_aux->filename = redir_tmp;
-//		 	}
-				redir_aux->fname = expand_quote_rm(redir_aux->fname, envlist);
-		 	redir_aux = redir_aux->next;
-		}
+		should_expand_core(cmd_aux, envlist);
 		cmd_aux = cmd_aux->next;
 	}
 }
